e6e3891c0670c8d52ad0fdeef2993a5527c1e0e0
JunYongPark <jypark@hotseleer.co.kr>
2025-04-28 21:19:47 +0900

    init

---
+++ demo/src/test/java/com/searcproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searcproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
